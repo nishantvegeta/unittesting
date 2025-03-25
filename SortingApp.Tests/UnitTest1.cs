43994eddcb2f38fbc759e3642f2e57cd051e1555
nishantvegeta <nishantkumpakha03@gmail.com>
2025-03-25 11:16:06 +0545

    feat: added 2 test

--- SortingApp.Tests/UnitTest1.cs
+++ SortingApp.Tests/UnitTest1.cs
@@ -19,4 +19,35 @@ public class UnitTest1
         // Assert
         Assert.Equal(expectedArray, result);
     }
+
+    [Fact]
+    public void Test_BubbleSort_SortsAlreadySortedArrayCorrectly()
+    {
+        // Arrange
+        var sortingApp = new SortingService();
+        int[] inputArray = { 11, 12, 22, 25, 34, 64, 90 };
+        int[] expectedArray = { 11, 12, 22, 25, 34, 64, 90 };
+
+        // Act
+        int[] result = sortingApp.BubbleSort(inputArray);
+
+        // Assert
+        Assert.Equal(expectedArray, result);
+    }
+
+    [Fact]
+    public void Test_BubbleSort_SortsArrayWithNegativeNumbersCorrectly()
+    {
+        // Arrange
+        var sortingApp = new SortingService();
+        int[] inputArray = { -64, 34, 25, -12, 22, 11, 90 };
+        int[] expectedArray = { -64, -12, 11, 22, 25, 34, 90 };
+
+        // Act
+        int[] result = sortingApp.BubbleSort(inputArray);
+
+        // Assert
+        Assert.Equal(expectedArray, result);
+    }
+
 }
